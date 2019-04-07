6909baae654ff2e9e6d117e8cae66a7cd6f3876d
Code Hz <CodeHz@outlook.com>
2019-04-07 12:54:01 +0800

    switch to use musl-clang

--- build-libfuse.sh
+++ build-libfuse.sh
@@ -4,4 +4,4 @@ tgt=$2
 if [ -e build.ninja ]; then
   rm -rf ./*
 fi
-CFLAGS="-I${src}/shim -Os" meson --prefix="${tgt}/libfuse" --default-library=static -Dutils=false "${src}/libfuse"
+CFLAGS="-I${src}/shim -Os -Wno-unused-command-line-argument" meson --prefix="${tgt}/libfuse" --default-library=static -Dutils=false "${src}/libfuse"
